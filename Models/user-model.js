3096013f8530cc71ae4614aaa08ec6b87194a86b
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-10-01 20:10:51 +0530

    user schema complete

---
+++ Models/user-model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["user", "seller"],
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+  },
+  profileimg:{
+    type:String,
+    default:"",
+  }
+
+});
