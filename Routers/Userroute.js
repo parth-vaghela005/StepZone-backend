950d7701ef76ba0c15357c0760261b19f4636fc5
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-10-02 10:38:44 +0530

    user router complete

--- Routers/Userroute.js
+++ Routers/Userroute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {RegisterUser,LoginUser} = require('../Controllers/usercontroller.js');
+const {RegisterUser,LoginUser,LogoutUser} = require('../Controllers/usercontroller.js');
 router.post('/Registration', RegisterUser);
 router.post('/Login', LoginUser);
+router.get('/Logout',LogoutUser)
 module.exports = {
     router
 }
