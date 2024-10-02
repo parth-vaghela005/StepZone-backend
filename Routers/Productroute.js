d8f2f6b697c3661040a80d41ca431eb1eccf1fc0
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-10-02 22:24:42 +0530

    complete routes for product controllers

---
+++ Routers/Productroute.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const {createProduct,getProductById} = require('../Controllers/productcontroller.js')
+const isAuth = require('../Middlewares/isAuth.js')
+const {AddReview} = require('../Controllers/Ratingcontroller.js')
+const upload = require('../utils/upload.js'); 
+const productrouter = express.Router()
+productrouter.post('/add', 
+    upload.fields([
+        { name: 'mainImage', maxCount: 1 }, 
+        { name: 'additionalImages', maxCount: 5 }
+    ]), isAuth,
+    createProduct
+);
+productrouter.post('/:id/review',isAuth,AddReview)
+productrouter.get('/:id',getProductById)
+module.exports = productrouter
