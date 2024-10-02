a5d727624763bb3bb7847f7ed5cf5d4c34b97883
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-10-02 22:22:12 +0530

    add controller for product review

---
+++ Controllers/Ratingcontroller.js
@@ -0,0 +1,26 @@
+const Rating = require('../Models/rating-model.js')
+const Product = require('../Models/product-model.js')
+const AddReview = async (req, res) => {
+    try {
+        const author = req.user.id
+        const { stars, description } = req.body
+        const product = await Product.findOne({ _id: req.params.id })
+        const review = new Rating({
+            stars,
+            description,
+            user: author,
+            product: req.params.id
+        })
+        await review.save()
+        product.rating.push(review._id)
+        product.save()
+        console.log(review);
+        res.status(200).json({ message: 'Review Added Successfully' })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Server error', error });
+    }
+}
+module.exports = {
+    AddReview
+}
