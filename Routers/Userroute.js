fb1a6e6261516f5d8ba6dad951b8c2ba6b010f9e
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-10-02 22:25:09 +0530

    complete routes for user controller

--- Routers/Userroute.js
+++ Routers/Userroute.js
@@ -1,16 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const upload = require('../utils/upload.js'); // Adjust path as needed
+const isAuth = require('../Middlewares/isAuth.js')
 const { createProduct } = require('../Controllers/productcontroller.js');
 const { RegisterUser, LoginUser, LogoutUser} = require('../Controllers/usercontroller.js')
 router.post("/Registration",RegisterUser)
 router.post("/Login",LoginUser)
 router.get("/Logout",LogoutUser)
-router.post('/create', 
-    upload.fields([
-        { name: 'mainImage', maxCount: 1 }, 
-        { name: 'additionalImages', maxCount: 5 }
-    ]), 
-    createProduct
-);
 module.exports = {router};
