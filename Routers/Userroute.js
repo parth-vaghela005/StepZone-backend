7cce2f531c9e283f79b092a5becb99be46a887b1
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-10-06 19:46:46 +0530

    add api routes for sendotp,verifyopt and changepassword

--- Routers/Userroute.js
+++ Routers/Userroute.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const isAuth = require('../Middlewares/isAuth.js')
-const { createProduct } = require('../Controllers/productcontroller.js');
-const { RegisterUser, LoginUser, LogoutUser} = require('../Controllers/usercontroller.js')
+// const { createProduct } = require('../Controllers/productcontroller.js');
+const { RegisterUser, LoginUser, LogoutUser,SendOtp,VerifyOtp} = require('../Controllers/usercontroller.js')
 router.post("/Registration",RegisterUser)
 router.post("/Login",LoginUser)
 router.get("/Logout",LogoutUser)
+router.post("/otp",isAuth,SendOtp)
+router.post("/verify",isAuth,VerifyOtp)
+router.post('/changepassword',isAuth,changePassword)
 module.exports = {router};
