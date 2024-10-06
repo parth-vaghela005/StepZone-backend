8b07301b53e3f72498a930d6eeeb811fdc5826c5
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-10-06 19:45:32 +0530

    add sendotp,verifyotp and changepassword controller

--- Controllers/usercontroller.js
+++ Controllers/usercontroller.js
@@ -1,6 +1,8 @@
 const User = require("../Models/user-model.js");
 const bcrypt = require("bcryptjs");
+const { authenticator } = require('otplib');
 const jwt = require("jsonwebtoken");
+const sendOTPEmail = require('../utils/sendOTPEmail.js')
 const RegisterUser = async (req, res) => {
   const { name, email, phone, password, role } = req.body;
   try {
@@ -35,7 +37,6 @@ const RegisterUser = async (req, res) => {
     console.log(error);
   }
 };
-
 const LoginUser = async (req, res) => {
   const { email, password } = req.body;
   try {
@@ -65,10 +66,10 @@ const LoginUser = async (req, res) => {
       { expiresIn: "1h" }
     );
     res.cookie("token", token, {
-      httpOnly: true, 
+      httpOnly: true,
       secure: true,
-      maxAge: 60 * 60 * 1000, 
-      sameSite: "Strict", 
+      maxAge: 60 * 60 * 1000,
+      sameSite: "Strict",
     });
     return res.status(200).json({
       success: true,
@@ -100,9 +101,115 @@ const LogoutUser = (req, res) => {
     message: "Logged out successfully",
   });
 };
-module.exports = { LogoutUser };
+const SendOtp = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found with this email address."
+      });
+    }
+    if (user._id.toString() !== req.user.id) {
+      console.log(user._id, req.user.id);
+      return res.status(401).json({
+        success: false,
+        message: "Your account is not registered with this email."
+      });
+    }
+    const secret = authenticator.generateSecret();
+    const otp = authenticator.generate(secret);
+    if (await sendOTPEmail(email, otp)) {
+      user.otps.push({ otp, createdAt: Date.now() });
+      await user.save();
+      return res.status(200).json({
+        success: true,
+        message: "OTP sent successfully.",
+        otp,
+        navigate: "naviagate otp form"
+      });
+    } else {
+      return res.status(500).json({
+        success: false,
+        message: "Failed to send OTP email."
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ success: false, message: "Server error." });
+  }
+};
+const VerifyOtp = async (req, res) => {
+  try {
+    const { otp } = req.body
+    const user = await User.findOne({ _id: req.user.id });
+    const latestOtp = user.otps.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))[0];
+    console.log(
+      "latestOtp",
+      latestOtp.otp
+    );
+    if (otp == latestOtp.otp) {
+      return res.status(201).json({
+        success: true,
+        message: "otp verifying successfully",
+        navigate: "chage password form"
+      })
+    } else {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid OTP."
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ success: false, message: "Server error." });
+  }
+}
+const changePassword = async (req, res) => {
+  try {
+    const { oldpassword, newpassword, retypepassword } = req.body;
+    if (!oldpassword || !newpassword || !retypepassword) {
+      return res.status(400).json({
+        success: false,
+        message: "Please fill all fields."
+      });
+    }
+    if (newpassword !== retypepassword) {
+      return res.status(400).json({
+        success: false,
+        message: "Password and confirm password do not match."
+      });
+    }
+    const user = await User.findOne({ _id: req.user.id });
+    if (!user) {
+      return res.status(404).json({ success: false, message: "User not found." });
+    }
+    const isValidPassword = await bcrypt.compare(oldpassword, user.password);
+    if (!isValidPassword) {
+      return res.status(401).json({ success: false, message: "Invalid password." });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(newpassword, salt);
+    user.password = hashedPassword;
+    await user.save();
+    return res.status(200).json({
+      success: true,
+      message: "Password changed successfully."
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Server error."
+    });
+  }
+};
 module.exports = {
   RegisterUser,
   LoginUser,
   LogoutUser,
+  SendOtp,
+  VerifyOtp,
+  changePassword
 };
