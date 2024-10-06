97a145a0bb841d5352fa7554b8f7c5eda18eb77c
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-10-06 19:45:52 +0530

    add some changes in user-model

--- Models/user-model.js
+++ Models/user-model.js
@@ -28,7 +28,17 @@ const UserSchema = new mongoose.Schema({
   profileimg:{
     type:String,
     default:"",
-  }
+  },
+  otps: [{
+        otp: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now, 
+        },
+}]
 
 });
  const  User  = mongoose.model("User",UserSchema)
