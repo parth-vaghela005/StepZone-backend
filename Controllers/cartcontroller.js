ba7ee2b598cffee88c3ab59dc5fa957d2d96331c
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-10-06 19:44:59 +0530

    add remove cart controller

--- Controllers/cartcontroller.js
+++ Controllers/cartcontroller.js
@@ -1,9 +1,5 @@
 
-const Product = require('../Models/product-model');
-// const Cart = require('../Models/cart-model');
-// const Product = require('../Models/product-model');
 const Cart = require('../Models/cart-model');
-
 const AddtoCart = async (req, res) => {
     try {
         const cartItem = await Cart.findOne({
@@ -12,17 +8,16 @@ const AddtoCart = async (req, res) => {
         if (!cartItem) {
             const newCart = new Cart({
                 user: req.user.id,
-                items: [{ productId: req.params.id, quantity: 1 }] // Initialize quantity to 1
+                items: [{ productId: req.params.id, quantity: 1 }] 
             });
             await newCart.save();
             return res.status(201).json({
                 message: 'Product added to cart',
                 cart: newCart
             });
-        } 
+        }
         const existingProduct = cartItem.items.find(item => item.productId.toString() === req.params.id);
         if (existingProduct) {
-            // If product exists, increase its quantity
             existingProduct.quantity += 1;
             await cartItem.save();
             return res.status(200).json({
@@ -32,7 +27,7 @@ const AddtoCart = async (req, res) => {
         } else {
             cartItem.items.push({
                 productId: req.params.id,
-                quantity: 1 
+                quantity: 1
             });
             await cartItem.save();
             return res.status(200).json({
@@ -44,7 +39,24 @@ const AddtoCart = async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 }
+const RemovetoCart = async (req, res) => {
+    try {
+        const productId = req.params.id;
+        const cartItem = await Cart.updateOne(
+            { user: req.user.id },
+            { $pull: { items: { productId } } }
+        );
+        const responseMessage = cartItem.modifiedCount > 0
+            ? "Product removed from cart"
+            : "Product not found in cart";
+        const statusCode = cartItem.modifiedCount > 0 ? 200 : 404;
+        return res.status(statusCode).json({ message: responseMessage, cart: cartItem });
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+}
 module.exports = {
-    AddtoCart
+    AddtoCart,
+    RemovetoCart
 };
 
