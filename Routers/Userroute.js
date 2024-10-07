061e4f9e9a1273a7330262a50003fe6cd1e60038
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-10-07 10:40:37 +0530

    add api route for edit profile

--- Routers/Userroute.js
+++ Routers/Userroute.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const isAuth = require('../Middlewares/isAuth.js')
-// const { createProduct } = require('../Controllers/productcontroller.js');
-const { RegisterUser, LoginUser, LogoutUser,SendOtp,VerifyOtp} = require('../Controllers/usercontroller.js')
+const upload = require('../Middlewares/upload.js');
+const { RegisterUser, LoginUser, LogoutUser,SendOtp,VerifyOtp,changePassword,editProfile} = require('../Controllers/usercontroller.js')
 router.post("/Registration",RegisterUser)
 router.post("/Login",LoginUser)
 router.get("/Logout",LogoutUser)
 router.post("/otp",isAuth,SendOtp)
 router.post("/verify",isAuth,VerifyOtp)
 router.post('/changepassword',isAuth,changePassword)
+router.post('/editprofile',isAuth,upload.single('profileimg'),editProfile)
 module.exports = {router};
